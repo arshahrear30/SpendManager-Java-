f21acde779b0d5b587fb3f9e1f87bf5604048cba
A R Shahrear <arshahrear30@gmail.com>
2025-09-28 19:16:57 +0600

    Update DatabaseHelper.java 3030

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -2,18 +2,22 @@ package com.arshahrear.spendmanager;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+//1 java class টা open হলে public class DatabaseHelper এর পাশে extends SQLiteOpenHelper লিখো enter করো
+//2 red bulb>> implement method >>ok || Again red bulb>> Create constructor >>ok
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context) {
-        super(context, "digital_moneybag", null, 1);
+    public DatabaseHelper(Context context) { // 3 সব কেটে দিয়ে শুধু Context context লিখবো
+
+        super(context, "digital_moneybag", null, 1); //4 Database file name digital_moneybag set করলাম 1st version
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db) { //5 table create
 
         db.execSQL("create table expense(id INTEGER primary key autoincrement,amount DOUBLE,reason TEXT,time DOUBLE)");
         db.execSQL("create table income(id INTEGER primary key autoincrement,amount DOUBLE,reason TEXT,time DOUBLE)");
@@ -21,14 +25,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     }
 
-    @Override
+    @Override //6
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //onUpgrade() মেথডটা তখনই কল হয়, যখন আপনার অ্যাপের ডাটাবেস version number পরিবর্তন করা হয়।
         //পুরনো টেবিল ফেলে দিয়ে নতুন টেবিল তৈরি করার জন্য DROP TABLE IF EXISTS ব্যবহার করা হয়।
         db.execSQL("DROP TABLE IF EXISTS expense");
+        db.execSQL("DROP TABLE IF EXISTS income");
 
     }
-// এতক্ষণ তো ডাটাবেসের টেবিল তৈরি করেছি এখন ইনপুট সিস্টেমটা করব addExpense method
+
+
+    //7--------------------------------------------এতক্ষণ ডাটাবেসের টেবিল তৈরি করেছি এখন database ইনপুট করব addExpense method
     public void addExpense(double amount, String reason){
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues conval=new ContentValues();
@@ -37,9 +44,54 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         conval.put("time",System.currentTimeMillis()); //Chrome search: android get time in milliseconds
 
         db.insert("expense",null,conval);
-        db.close();
 
     }
+    //--------------------------------------------
+
+    //8 same--------------------------------------------database ইনপুট  addIncome method
+    public void addIncome(double amount, String reason){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues conval=new ContentValues();
+        conval.put("amount",amount);
+        conval.put("reason",reason);
+        conval.put("time",System.currentTimeMillis()); //Chrome search: android get time in milliseconds
+
+        db.insert("income",null,conval);
+
+    }
+    //--------------------------------------------
+
+
+    //9 ============================================খরচ টাকা table যুক্ত করছি।টোটাল টাকা দেখানোর জন্য select from দিয়ে পুরা expense টেবিল ধরছি নিচে loop দিয়ে যোগ করছি
+    public double calculateTotalExpense() {
+        double totalExpense=0;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(  "select * from expense",  null);
+        if (cursor!=null && cursor.getCount()>0){
+            while ( cursor.moveToNext() ) {
+                double amount = cursor.getDouble(  1);
+                totalExpense = totalExpense+amount;
+            }
+        }
+        return totalExpense;
+    }
+//============================================
+
+
+    //10 same============================================ইনকাম টাকা যে যুক্ত করছি সেটা টোটাল টাকা দেখানোর জন্য
+    public double calculateTotalIncome() {
+        double totalIncome=0;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(  "select * from income",  null);
+        if (cursor!=null && cursor.getCount()>0){
+            while ( cursor.moveToNext() ) {
+                double amount = cursor.getDouble(  1);
+                totalIncome = totalIncome+amount;
+            }
+        }
+        return totalIncome;
+    }
+//============================================
 
 
 }
