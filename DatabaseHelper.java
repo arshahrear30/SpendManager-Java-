3ec429e1e274b37e4c45c4fcf640e7646d7069c8
A R Shahrear <arshahrear30@gmail.com>
2025-10-06 00:14:50 +0600

    Update DatabaseHelper.java  3033

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -1,5 +1,5 @@
 package com.arshahrear.spendmanager;
-
+//DatabaseHelper.java
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -10,11 +10,16 @@ import androidx.annotation.Nullable;
 
 //1 java class টা open হলে public class DatabaseHelper এর পাশে extends SQLiteOpenHelper লিখো enter করো
 //2 red bulb>> implement method >>ok || Again red bulb>> Create constructor >>ok
+
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context context) { // 3 সব কেটে দিয়ে শুধু Context context লিখবো
 
         super(context, "digital_moneybag", null, 1); //4 Database file name digital_moneybag set করলাম 1st version
     }
+
+
+
 
     @Override
     public void onCreate(SQLiteDatabase db) { //5 table create
@@ -22,8 +27,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("create table expense(id INTEGER primary key autoincrement,amount DOUBLE,reason TEXT,time DOUBLE)");
         db.execSQL("create table income(id INTEGER primary key autoincrement,amount DOUBLE,reason TEXT,time DOUBLE)");
 
-
     }
+
+
 
     @Override //6
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -48,7 +54,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     //--------------------------------------------
 
-    //8 same--------------------------------------------database ইনপুট  addIncome method
+    //7.1 same--------------------------------------------database ইনপুট  addIncome method
     public void addIncome(double amount, String reason){
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues conval=new ContentValues();
@@ -62,39 +68,58 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //--------------------------------------------
 
 
-    //9 ============================================খরচ টাকা table যুক্ত করছি।টোটাল টাকা দেখানোর জন্য select from দিয়ে পুরা expense টেবিল ধরছি নিচে loop দিয়ে যোগ করছি
+//8 ============================================Expense টাকা table যুক্ত করছি।টোটাল টাকা দেখানোর জন্য select from দিয়ে পুরা expense টেবিল ধরছি নিচে loop দিয়ে যোগ করছি
     public double calculateTotalExpense() {
+
         double totalExpense=0;
+
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(  "select * from expense",  null);
+
         if (cursor!=null && cursor.getCount()>0){
-            while ( cursor.moveToNext() ) {
+
+
+
+            while ( cursor.moveToNext() )
+
+            {
                 double amount = cursor.getDouble(  1);
                 totalExpense = totalExpense+amount;
             }
+
+
+
         }
+
         return totalExpense;
     }
 //============================================
 
 
-    //10 same============================================ইনকাম টাকা যে যুক্ত করছি সেটা টোটাল টাকা দেখানোর জন্য
+//8.1 same============================================ইনকাম টাকা যে যুক্ত করছি সেটা টোটাল টাকা দেখানোর জন্য
     public double calculateTotalIncome() {
+
         double totalIncome=0;
+
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(  "select * from income",  null);
+
         if (cursor!=null && cursor.getCount()>0){
+
+
             while ( cursor.moveToNext() ) {
                 double amount = cursor.getDouble(  1);
                 totalIncome = totalIncome+amount;
             }
+
+
         }
+
         return totalIncome;
     }
 //============================================
 
-    //\\\\\\\\\\\\\\\\\\\\Data show করার জন্য showAllData method create করছি । Database table থাকা data cursor হিসেবে return করবে
-
+    //9\\\\\\\\\\\\\\\\\\\\Expense show করার জন্য showAllExpense method create করছি । Database table থাকা data cursor হিসেবে return করবে
     public Cursor showAllExpense(){
         SQLiteDatabase db=this.getReadableDatabase();
         Cursor cursor=db.rawQuery("select * from expense",null);
@@ -104,49 +129,40 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     }
 
-
     //\\\\\\\\\\\\\\\\\\\\
 
 
 
-    //\\\\\\\\\\\\\\\\\\\\Data show করার জন্য showAllData method create করছি । Database table থাকা data cursor হিসেবে return করবে
-
+    //9.1\\\\\\\\\\\\\\\\\\\\Income show করার জন্য showAllIncome method create করছি । Database table থাকা data cursor হিসেবে return করবে
     public Cursor showAllIncome(){
         SQLiteDatabase db=this.getReadableDatabase();
         Cursor cursor=db.rawQuery("select * from income",null);
         return cursor;
 
-
-
     }
-
 
     //\\\\\\\\\\\\\\\\\\\\
 
-    //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 
+
+    //10  ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd table থেকে data delete করার জন্য
     public void deleteExpense(String id){
-
 
         SQLiteDatabase db=this.getWritableDatabase();
         db.execSQL("DELETE FROM expense WHERE id = " + id);
 
     }
-
-
     //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 
-    //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 
+
+    //10.1  ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd  table থেকে data delete করার জন্য
     public void deleteIncome(String id){
-
 
         SQLiteDatabase db=this.getWritableDatabase();
         db.execSQL("DELETE FROM income WHERE id = " + id);
 
     }
-
-
     //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 
 
